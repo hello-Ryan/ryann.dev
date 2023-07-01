cebfc26ed0bd9bb41d6399897eb6116049812cdd
hleu0019 <ryanleung1029@gmail.com>
2023-07-01 15:53:48 +1000

    remove un needed imports

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,11 +1,7 @@
-import { Navbar } from "@/components";
-
 const Experience = () => {
   return (
     <div>
-      <p className="flex grow flex-col justify-start text-3xl text-white">
-        Currently WIP
-      </p>
+      <p className="text-3xl text-white">Currently WIP</p>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
-import { Navbar, Landing, ContactMe } from "@/components";
+import { Landing } from "@/components";
 import { type NextPage } from "next";
-import { useCallback, useState } from "react";
 
 const Home: NextPage = () => {
   return (
     <>
-      <Landing className="flex grow flex-col justify-start" />
+      <Landing className="" />
     </>
   );
 };
