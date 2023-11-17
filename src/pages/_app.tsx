70649c202f25b3e1f63e3c59225ac58b7031f803
hleu0019 <ryanleung1029@gmail.com>
2023-11-17 16:59:53 +1100

    delete current

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,12 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
-import { useCallback, useState } from "react";
-import { ContactMe, Navbar } from "@/components";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  const [showContactForm, setShowContactForm] = useState(false);
-
-  const toggleShowModal = useCallback(() => {
-    setShowContactForm((current) => !current);
-  }, []);
 
   return (
     <>
-      <ContactMe open={showContactForm} setOpen={toggleShowModal} />
-      <div className={`${showContactForm ? "bg-gray-800 opacity-40" : ""}`}>
-        <div className="flex h-screen flex-col">
-          <Navbar toggleForm={toggleShowModal} />
-          <div className="px-48">
-            <Component {...pageProps} />
-          </div>
-        </div>
-      </div>
-
+      <Component {...pageProps} />
       <Analytics />
     </>
   );
--- src/pages/projects.tsx
+++
@@ -1,9 +0,0 @@
-const Projects = () => {
-  return (
-    <div>
-      <p className="text-3xl text-white">Currently WIP</p>
-    </div>
-  );
-};
-
-export default Projects;
