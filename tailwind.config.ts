f36d25a4c6f910f5ee2c21df125034bd0034cdc3
hleu0019 <ryanleung1029@gmail.com>
2023-11-27 19:28:36 +1100

    fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,14 @@
 import { type Config } from "tailwindcss";
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
     darkMode: "class",
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
+            fontFamily: {
+                RobotoMono: ['RobotoMono', ...defaultTheme.fontFamily.sans]
+            },
             animation: {
                 cursor: "cursor .6s linear infinite alternate",
                 type: "type 1.8s ease-out .8s 1 normal both",
