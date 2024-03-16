b9e20ba74d8e62aec6e1da896418305f15f86769
hleu0019 <ryanleung1029@gmail.com>
2024-03-16 23:40:39 +1100

    comment out

--- src/components/StarBackground.tsx
+++ src/components/StarBackground.tsx
@@ -7,25 +7,24 @@ import { Camera, Color, Vector2, Vector3 } from "three";
 
 const StarBackground = () => {
     const lightBlue = new Color("rgb(0, 255, 255)")
-    const Parralax = () => {
-        const { camera, mouse } = useThree();
+    // const Parralax = () => {
+    //     const { camera, mouse } = useThree();
 
-        const vec = new Vector3();
+    //     const vec = new Vector3();
 
-        return useFrame(() => {
-            camera.position.lerp(
-                vec.set(mouse.x, mouse.y , camera.position.z),
-                0.01
-            );
-            camera.lookAt(0, 0, 0);
-        });
-    };
+    //     return useFrame(() => {
+    //         camera.position.lerp(
+    //             vec.set(mouse.x, mouse.y , camera.position.z),
+    //             0.01
+    //         );
+    //         camera.lookAt(0, 0, 0);
+    //     });
+    // };
     return (
         <div className="h-full w-full">
             <Canvas camera={{ position: [0, 0, 0.5] }}>
                 <mesh>
-
-                    <Parralax />
+                    {/* <Parralax /> */}
                     <Sparkles count={100} color={lightBlue} speed={0.1}/>
                     <meshStandardMaterial />
                 </mesh>
